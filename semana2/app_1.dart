5849e91eb2416f4af133278b45f482b8fdd8182f
Ignacio246 <>
2021-05-27 02:21:11 +0000

    new folder

--- semana2/app_1.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  Map persona = {
-    "nombre" : "Ignacio",
-    "telefono" : 7751352402,
-    "email" : "test@gmail.com"
-  };
-  print (persona);
-  cn_val=int(raw_input("Ingrese Valor: "));
-}
